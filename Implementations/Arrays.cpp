8ecf21001f3fdaef6a6b985ac00233f2d39ace0a
oshuptrine <laxheadoliver@gmail.com>
2025-02-20 12:24:34 -0500

    Array Practice

---
+++ Implementations/Arrays.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include<iostream> // Header file for input/output stream
+using namespace std; // Standard namespace
+
+// February 21st, 2025
+// Array Practice
+
+int findLargest(int nums[], int n){
+// Return the maximum element in the array using max_element from algorithm library
+    return *max_element(nums, nums + n);
+}
+
+int main(){
+
+    // Array Initialization 
+    int nums[]={1,3,5,7,9,11,22,46,81,-21};
+
+    // Find the number of elements in the array
+    int n = sizeof(nums) / sizeof(nums[0]);
+
+    cout << "Original Array:";
+    for(int i = 0; i < n; i++)
+        cout << nums[i] << " ";
+        // Prints the largest element of the array 
+        cout << "\n The largest element in the array is:" << findLargest(nums, n);
+    
+    // terminates program after succesful run
+    return 0;
+
+}
